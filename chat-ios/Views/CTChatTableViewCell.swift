c91e6d58d4e9be3242a265449091b123cd67323d
Brian <briancorrea@gmail.com>
2016-06-16 21:37:34 -0400

    Custom TableView Cell

---
+++ chat-ios/Views/CTChatTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CTChatTableViewCell.swift
+//  chat-ios
+//
+//  Created by Brian Correa on 6/16/16.
+//  Copyright © 2016 Velocity 360. All rights reserved.
+//
+
+import UIKit
+
+class CTChatTableViewCell: UITableViewCell {
+    
+    static var cellId = "cellId"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
