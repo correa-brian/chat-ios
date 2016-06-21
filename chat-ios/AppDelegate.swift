91c81334a295e9d9f26233458b0bc92d10cb08ce
Brian <briancorrea@gmail.com>
2016-06-21 13:25:24 -0400

    Enabling Firebase

--- chat-ios/AppDelegate.swift
+++ chat-ios/AppDelegate.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        FIRApp.configure()
         
         let frame = UIScreen.mainScreen().bounds
         self.window = UIWindow(frame: frame)
