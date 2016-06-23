476d136fd9d17ca9e2832ba0bceade5eeba43704
Brian <briancorrea@gmail.com>
2016-06-22 21:46:42 -0400

    Creating CTPost Model

---
+++ chat-ios/Models/CTPost.swift
@@ -0,0 +1,13 @@
+//
+//  CTPost.swift
+//  chat-ios
+//
+//  Created by Brian Correa on 6/22/16.
+//  Copyright © 2016 Velocity 360. All rights reserved.
+//
+
+import UIKit
+
+class CTPost: NSObject {
+
+}
